336c36b8b3ab0446cc14e0054fc4e069584ba464
Jesus Munoz <57254954+Jumba123@users.noreply.github.com>
2025-08-14 03:14:26 -0600

    app.py

--- app.py
+++ app.py
@@ -28,20 +28,22 @@ def Pre_Process_CSV(df):
     return df
 
 def Home_Page (df):
-    df["PPG"] = df["Points"] / df["Games Played"]
-    df["PPG"] = df["PPG"].round(2)
-    df.insert(7, "PPG", df.pop("PPG"))
-    df = df.drop(columns=["Skater_ID", "Week", "Team","Goals Allowed"])
-
     Options = ["Points","PPG","Goals", "Assists"]
     Rank_Choice = st.pills("Rank By:", Options, default=Options[0], selection_mode="single")
 
     for option in Options:
         if Rank_Choice == option:
             df_temp = df.copy()
-
+            
             # Group and sort by the selected stat
-            df_temp = df_temp.groupby("Skaters", as_index=False).sum(option).sort_values(option, ascending=False)
+            df_temp = df_temp.groupby("Skaters", as_index=False).sum("Points")
+            
+            df_temp["PPG"] = df_temp["Points"] / df_temp["Games Played"]
+            df_temp["PPG"] = df_temp["PPG"].round(2)
+            df_temp.insert(7, "PPG", df_temp.pop("PPG"))
+            df_temp = df_temp.drop(columns=["Skater_ID", "Week","Goals Allowed"])
+            df_temp = df_temp.sort_values(option, ascending=False)
+
 
             # Games played filter
             slider_gp = st.slider("Games Played", 0, df_temp['Games Played'].max().round(0).astype(int), 0)
@@ -53,31 +55,6 @@ def Home_Page (df):
             df_temp = df_temp.set_index(f"Rank ({option})")
             st.dataframe(df_temp)
             break  # Stop after the matching option is processed
-
-
-    # if Rank_Choice == Options[0]:
-    #     df0 = df.copy()
-    #     df0 = df0.groupby("Skaters",as_index=False).sum(Options[0]).sort_values(Options[0],ascending=False)
-        
-    #     slider_gp = st.slider("Games Played", 0, df0['Games Played'].max().round(0).astype(int),0)
-    #     df0 = df0[df0['Games Played'] >= slider_gp]
-
-    #     df0['Rank'] = df0[Options[0]].rank(method='dense', ascending=False)
-    #     df0.insert(0, f"Rank ({Options[0]})", df0.pop("Rank"))
-    #     df0 = df0.set_index(f'Rank ({Options[0]})')
-    #     st.dataframe(df0)
-    # elif Rank_Choice == Options[1]:
-    #     df1 = df.copy()
-    #     df1 = df1.groupby("Skaters",as_index=False).sum(Options[1]).sort_values(Options[0],ascending=False)
-        
-    #     slider_gp = st.slider("Games Played", 0, df1['Games Played'].max().round(0).astype(int),0)
-        
-    #     df1 = df1[df1['Games Played'] >= slider_gp]
-    #     df1['Rank'] = df1[Options[1]].rank(method='dense', ascending=False)
-    #     df1.insert(0, f"Rank ({Options[1]})", df1.pop("Rank"))
-    #     df1 = df1.set_index(f'Rank ({Options[1]})')
-    #     st.dataframe(df1)
-
 
 df = Pre_Process_CSV(df)
 st.markdown(f"<h1 style='text-align: center;'>PHL D4 Data (Week 1-3 Not Included)</h1>", unsafe_allow_html=True)
@@ -97,4 +74,3 @@ if Weekly_Link:
     st.switch_page("pages/3_weekly_stats.py")
 
 Home_Page(df)
-
